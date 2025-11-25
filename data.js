3ded89d015e2b1df0cd0bf7b44a1a5cb82c71c85
JamesMcArrow <jakub.makarov@gmail.com>
2025-11-25 22:15:06 +0100

    stul 02 added

--- data.js
+++ data.js
@@ -2,6 +2,7 @@ const catalogData = {
   items: [
     { title: "N01", model: "models/zidle_01/zidle_01.gltf" },
     { title: "N05", model: "models/stul_01/stul_01.gltf" },
+    { title: "N06", model: "models/stul_02/stul_02.gltf" },
     { title: "I01", model: "models/lampa_01/lampa_01.gltf" },
     { title: "I02", model: "models/lampa_02/lampa_02.gltf" },
   ]
