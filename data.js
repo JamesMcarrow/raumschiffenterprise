54ba836fcee194a102f487a627952c527b55fbbc
JamesMcArrow <jakub.makarov@gmail.com>
2025-11-25 18:47:45 +0100

    website reformated

---
+++ data.js
@@ -0,0 +1,8 @@
+const catalogData = {
+  items: [
+    { title: "N01", model: "models/zidle_01/zidle_01.gltf" },
+    { title: "N05", model: "models/stul_01/stul_01.gltf" },
+    { title: "I01", model: "models/lampa_01/lampa_01.gltf" },
+    { title: "I02", model: "models/lampa_02/lampa_02.gltf" },
+  ]
+};
