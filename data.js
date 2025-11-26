40996e8f82ab8377b4e6909a68610c72525e7c2a
JamesMcArrow <jakub.makarov@gmail.com>
2025-11-26 17:27:08 +0100

    stul_03 added, tex changes

--- data.js
+++ data.js
@@ -3,6 +3,7 @@ const catalogData = {
     { title: "N01", model: "models/zidle_01/zidle_01.gltf" },
     { title: "N05", model: "models/stul_01/stul_01.gltf" },
     { title: "N06", model: "models/stul_02/stul_02.gltf" },
+    { title: "N07", model: "models/stul_03/stul_03.gltf" },
     { title: "I01", model: "models/lampa_01/lampa_01.gltf" },
     { title: "I02", model: "models/lampa_02/lampa_02.gltf" },
   ]
